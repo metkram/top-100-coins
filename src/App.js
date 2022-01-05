00a2cd7d49866d602ce92ea5478cdf29bfbf6d1b
Andrew T <metkraming@gmail.com>
2022-01-05 23:47:32 +0200

    add materializecss framework

--- src/App.js
+++ src/App.js
@@ -9,19 +9,28 @@ function App() {
     });
   }, []);
   return (
-    <div>
-      <ol>
-        {coinsList && coinsList.map(coin => <Coin key={coin.id} name={coin.name} image={coin.image} price={coin.current_price} />)}
-      </ol>
+    <div className="container">
+      <div className="row">
+        <div className="col s1">#</div>
+        <div className="col s1"></div>
+        <div className="col s4">Name</div>
+        <div className="col s3">Marker Capitlization</div>
+        <div className="col s3">Current Price</div>
+      </div>
+      {coinsList && coinsList.map(coin => <div key={coin.id}><Coin rank={coin.market_cap_rank} cap={coin.market_cap} name={coin.name} image={coin.image} price={coin.current_price} /></div>)}
     </div>
   );
 }
 
-function Coin({key, name, image, price}) {
+function Coin({name, image, price, rank, cap}) {
   return (
-    <li key={key}>
-      <img src={image} width="30px" />{name} price: ${price}
-    </li>
+    <div className="row valign-wrapper">
+      <div className="col s1">{rank}</div>
+      <div className="col s1"><img src={image} width="30px" /></div>
+      <div className="col s4">{name}</div>
+      <div className="col s3">{cap}</div>
+      <div className="col s3">${price}</div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
 import App from './App';
 
+function Index() {
+  useEffect(() => {
+    const elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {});
+  }, []);
+  return (
+    <div>
+      <nav>
+        <div className="container">
+          <div className="nav-wrapper">
+            <a className="brand-logo">Coins</a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><a href="#">GitHub repo</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile-demo">
+        <li><a href="#">GitHub repo</a></li>
+      </ul>
+      <div className="container">
+        <h1>Top 100 crypto coins</h1>
+      </div>
+      <App />
+      <footer className="page-footer">
+        <div className="footer-copyright">
+          <div className="container">
+          Information is parsed using Coingecko API
+          <a className="grey-text text-lighten-4 right" href="https://www.coingecko.com/">Visit Coingecko</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
 
 ReactDOM.render(
-    <App />,
+    <Index />,
   document.getElementById('root')
 );
